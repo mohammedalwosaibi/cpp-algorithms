593a4eb5baa8512c6541b9282d97f3c860f25ee0
Mohammed Alwosaibi <mohammedalwosaibi@gmail.com>
2025-11-17 16:55:27 -0600

    Implement visualize_weighted_graph to display adjacency lists with edges

--- graph.cpp
+++ graph.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <utility>
+#include <tuple>
 #include <iostream>
 
 std::vector<std::vector<int>> unweighted_graph(int n, const std::vector<std::pair<int, int>>& edges) {
@@ -22,7 +23,7 @@ std::vector<std::vector<std::pair<int, int>>> weighted_graph(int n, const std::v
     return adj;
 }
 
-void visualize_unweighted_graph(std::vector<std::vector<int>>& adj) {
+void visualize_unweighted_graph(const std::vector<std::vector<int>>& adj) {
     for (int i = 0; i < adj.size(); i++) {
         std::cout << i << " -> ";
         for (int j = 0; j < adj[i].size(); j++) {
@@ -35,10 +36,23 @@ void visualize_unweighted_graph(std::vector<std::vector<int>>& adj) {
     }
 }
 
+void visualize_weighted_graph(const std::vector<std::vector<std::pair<int, int>>>& adj) {
+    for (int i = 0; i < adj.size(); i++) {
+        std::cout << i << " -> ";
+        for (int j = 0; j < adj[i].size(); j++) {
+            std::cout << "(" << adj[i][j].first << ", " << adj[i][j].second << ")";
+            if (j != adj[i].size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "\n";
+    }
+}
+
 int main() {
     int n = 5;
-    std::vector<std::pair<int, int>> edges = {{0, 1}, {1, 2}, {2, 4}, {3, 4}, {1, 3}};
-    std::vector<std::vector<int>> adj = unweighted_graph(n, edges);
-    visualize_unweighted_graph(adj);
+    std::vector<std::tuple<int, int, int>> edges = {{0, 1, 10}, {1, 2, 8}, {2, 4, 6}, {3, 4, 2}, {1, 3, 23}};
+    std::vector<std::vector<std::pair<int, int>>> adj = weighted_graph(n, edges);
+    visualize_weighted_graph(adj);
     return 0;
 }
