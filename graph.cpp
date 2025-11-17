fa42f874155108e46265748f0430b3aa3e13d02b
Mohammed Alwosaibi <mohammedalwosaibi@gmail.com>
2025-11-17 16:27:53 -0600

    Implement weighted graph to convert weighted edges into an adjacency list

--- graph.cpp
+++ graph.cpp
@@ -1,10 +1,20 @@
 #include <vector>
 
-std::vector<std::vector<int>> unweighted_graph(int n, std::vector<std::pair<int, int>>& edges) {
+std::vector<std::vector<int>> unweighted_graph(int n, const std::vector<std::pair<int, int>>& edges) {
     std::vector<std::vector<int>> adj(n);
 
-    for (auto& [out, in] : edges) {
+    for (const auto& [out, in] : edges) {
         adj[out].push_back(in);
+    }
+
+    return adj;
+}
+
+std::vector<std::vector<std::pair<int, int>>> weighted_graph(int n, const std::vector<std::tuple<int, int, int>>& edges) {
+    std::vector<std::vector<std::pair<int, int>>> adj(n);
+
+    for (const auto& [out, in, weight] : edges) {
+        adj[out].push_back({in, weight});
     }
 
     return adj;
